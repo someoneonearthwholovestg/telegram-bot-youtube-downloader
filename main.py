98c8bbffbee90a965ca0d91db7d047513996d27a
anandvfcvijay <49161738+anandvfcvijay@users.noreply.github.com>
2019-05-04 14:41:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def download_choosen_format(bot, update):
             bot.send_document(chat_id=query.message.chat_id, document=open(f, 'rb'))
 
 
-updater = Updater(token=YOUR_TOKEN)
+updater = Updater(token=878465109:AAFVs8YEBxnyff3smDmXVRp4Dj7bEWEmMl0)
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, get_format))
 updater.dispatcher.add_handler(CallbackQueryHandler(download_choosen_format))
